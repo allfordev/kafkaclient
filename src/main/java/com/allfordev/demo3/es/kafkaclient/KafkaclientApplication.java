4505e220e01d34d9e5129950137c4561f2a70a6c
Dan Allford <development@danallford.com>
2024-05-15 10:41:39 +0100

    initial commit

---
+++ src/main/java/com/allfordev/demo3/es/kafkaclient/KafkaclientApplication.java
@@ -0,0 +1,13 @@
+package com.allfordev.demo3.es.kafkaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allfordev/demo3/es/kafkaclient/KafkaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allfordev.demo3.es.kafkaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
